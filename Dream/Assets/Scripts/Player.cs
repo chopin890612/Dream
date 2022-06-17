e92f77c420c0bc983284a7c822059f4aebea8839
chopin890612 <chopin890612@gmail.com>
2022-06-17 17:12:24 +0800

    Movement Remake Day8
    ClimbJump Complete, left attack and dash

--- Dream/Assets/Scripts/Player.cs
+++ Dream/Assets/Scripts/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
     [Header("Y Axis Movement")]
     [SerializeField] float jumpSpeed = 45f;
     [SerializeField] float climbSpeed = 45f;
-    [SerializeField] float fallSpeed = 45f;
+    [SerializeField] float fallSpeedLimiter = 45f;
     [Space(5)]
 
     [Header("Ground Settings")]
@@ -31,7 +31,6 @@ public class Player : MonoBehaviour
     [SerializeField] bool onWall = false;
     [SerializeField] bool isWallJumping = false;
     [SerializeField] float wallJumpSpeedx;
-    [SerializeField] float wallJumpSpeedy;
     [Space(5)]
 
     [Header("Debugger")]
@@ -59,6 +58,7 @@ public class Player : MonoBehaviour
     private float _playerDirection;
     private GameObject _spineRenderer;
     private bool _isWallOnRight =true;
+    private Transform _wallTransform;
 
     
     private void Awake()
@@ -89,42 +89,36 @@ public class Player : MonoBehaviour
 
         _animator.SetBool("IsWallJumping", isWallJumping);
 
-        _animator.SetBool("IsMoveToWall", _isWallOnRight == faceRight);
+        _animator.SetBool("IsMoveToWall", _isWallOnRight);
     }
 
     private void FixedUpdate()
     {
+        //Custom Gravity
         _rb.AddForce(_gravity * _gravityScale, ForceMode.Acceleration);
-        if (_rb.velocity.y < fallSpeed)
-            _rb.velocity = new Vector2(_rb.velocity.x, fallSpeed);
-
+        if (_rb.velocity.y < fallSpeedLimiter)
+            _rb.velocity = new Vector2(_rb.velocity.x, fallSpeedLimiter);
+        
+        //Ground Update
         onGround = Physics.SphereCast(transform.position + new Vector3(0, anchorOffset, 0), groundDetectRadius, Vector3.down,
             out RaycastHit hitGround, groundDetectDistance, LayerMask.GetMask("Ground"));
+        if (onGround == false)
+            _flyTime += Time.deltaTime;
+        if (isWalking)
+            _skeletonAnimation.timeScale = Mathf.Abs(_playerDirection);
+        if (!isWallJumping)
+            _rb.velocity = new Vector3(_playerDirection * moveSpeed, _rb.velocity.y);
 
+        //Wall Update
         onWall = Physics.BoxCast(transform.position + new Vector3(0, anchorOffset, 0), wallDetectRadius / 2, faceRight? Vector3.right : Vector3.left,
                  out RaycastHit hitwall, Quaternion.identity, wallDetectDistance, LayerMask.GetMask("Wall"));
         if (onWall)
-            _isWallOnRight = faceRight;
-
-        if(!isWallJumping)
-            _rb.velocity = new Vector3(_playerDirection * moveSpeed, _rb.velocity.y);
-        if (isWalking)
-            _skeletonAnimation.timeScale = Mathf.Abs(_playerDirection);
-
-        if (onGround == false)
-            _flyTime += Time.deltaTime;
-
-        //if (isUpdateClimbJump)
-        //{
-        //    var downVelocity = Mathf.Min(_rb.velocity.y, 0);
-        //    var deltaVelocity = new Vector3(0, jumpSpeed - downVelocity, 0);
-        //    _rb.AddForce(deltaVelocity, ForceMode.VelocityChange);
-        //}
-
-
+        {            
+            _wallTransform = hitwall.transform;
+            WallFace();
+        }
 
         PlayerFlip();
-
 
         //Debugger
         if (hitGround.distance == 0f)
@@ -136,6 +130,26 @@ public class Player : MonoBehaviour
             currentBoxDistance = wallDetectDistance;
         else
             currentBoxDistance = hitwall.distance;
+    }
+    private void WallFace()
+    {
+        if ((_wallTransform.position - transform.position).x > 0)
+            _isWallOnRight = true;
+        else if ((_wallTransform.position - transform.position).x < 0)
+            _isWallOnRight = false;
+    }
+    private void PlayerFlip()
+    {
+        if (_playerDirection > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            faceRight = true;
+        }
+        else if (_playerDirection < 0)
+        {
+            transform.localScale = Vector3.one;
+            faceRight = false;
+        }
     }
 
     private void OnDrawGizmosSelected()
@@ -149,39 +163,58 @@ public class Player : MonoBehaviour
     {
         switch (stateName)
         {
+            //#################### Jump ############################
             case "StartJump":
                 StartJump();
                 break;
             case "EndJump":
                 EndJump();
                 break;
+
+            //#################### Idle ############################
             case "StartIdle":
                 StartIdle();
                 break;
+
+            //#################### Walk ############################
             case "StartWalk":
                 StartWalk();
                 break;
             case "EndWalk":
                 EndWalk();
                 break;
+
+            //#################### Climb ###########################
             case "StartClimb":
                 StartClimb();
                 break;
             case "EndClimb":
                 EndClimb();
                 break;
+
+            //ClimbJump
             case "StartClimbJump":
                 StartClimbJump();
+                break;
+            case "ClimbJump":
+                ClimbJump();
                 break;
             case "EndClimbJump":
                 EndClimbJump();
                 break;
-            case "KeepClimbJumpPhase":
-                KeepClimbJumpPhase();
+
+            //ClimbWall
+            case "StartClimbWall":
+                StartClimbWall();
                 break;
-            case "EndKCJPhase":
-                EndKCJPhase();
+            case "ClimbWall":
+                ClimbWall();
                 break;
+            case "EndClimbWall":
+                EndClimbWall();
+                break;
+
+            //#################### Fall ############################
             case "Falling":
                 Falling();
                 break;
@@ -198,12 +231,15 @@ public class Player : MonoBehaviour
     {
         // 產生向上的力道來抵銷玩家角色任何向下的動量
         // 以讓玩家角色向上跳躍
+        Jump();
+        _flyTime = 0f;
+    }
+    private void Jump()
+    {
         var downVelocity = Mathf.Min(_rb.velocity.y, 0);
         var deltaVelocity = new Vector3(0, jumpSpeed - downVelocity, 0);
         _rb.AddForce(deltaVelocity, ForceMode.VelocityChange);
         _skeletonAnimation.AnimationState.SetAnimation(0, jump, false);
-
-        _flyTime = 0f;
     }
     private void EndJump()
     {
@@ -244,6 +280,7 @@ public class Player : MonoBehaviour
     {
         _gravityScale = 0;
         _rb.velocity = new Vector2(0, climbSpeed);
+        Debug.Log("Slide Wall");
     }
     private void EndClimb() 
     {
@@ -252,50 +289,58 @@ public class Player : MonoBehaviour
     private void StartClimbJump()
     {
         _rb.velocity = Vector3.zero;
-        _rb.AddForce(new Vector3(wallJumpSpeedx * (faceRight == true ? -1 : 1),wallJumpSpeedy,0), ForceMode.Impulse);
+        _rb.AddForce(new Vector3(wallJumpSpeedx * (_isWallOnRight == true ? -1 : 1),0,0), ForceMode.Impulse);
         isWallJumping = true;
-        Invoke("SetIsWallJumpingFalse", 0.1f);
+        Invoke("SetIsWallJumpingFalse", 0.15f);
         _flyTime = 0f;
         _gravityScale = 1;
-    }
-    private void EndClimbJump()
-    {
-        _gravityScale = 1f;
     }
     private void SetIsWallJumpingFalse()
     {
         isWallJumping = false;
     }
-    private void KeepClimbJumpPhase()
-    {
 
+    private void ClimbJump()
+    {
+        Jump();
     }
-    private void EndKCJPhase()
+    private void EndClimbJump()
     {
-
+        EndJump();
+    }
+    private void StartClimbWall()
+    {
+        _rb.velocity = Vector3.zero;
+        _rb.AddForce(new Vector3(wallJumpSpeedx * (_isWallOnRight == true ? -1 : 1), 0, 0), ForceMode.Impulse);
+        isWallJumping = true;
+        Invoke("ClimbWallCallback", 0.1f);
+        _flyTime = 0f;
+        _gravityScale = 1;
+    }
+    private void ClimbWallCallback()
+    {
+        _rb.AddForce(new Vector3(wallJumpSpeedx * (_isWallOnRight == true ? 1 : -1), 0, 0), ForceMode.Impulse);
+        SetIsWallJumpingFalse();
+    }
+    private void ClimbWall()
+    {
+        Jump();
+    }
+    private void EndClimbWall()
+    {
+        EndJump();
     }
 
     #endregion
+
+    #region Fall
 
     private void Falling()
     {
         _gravityScale = 1f;
-        //_rb.AddForce(new Vector2(0, fallSpeed), ForceMode.VelocityChange);
     }
 
     #endregion
 
-    private void PlayerFlip()
-    {
-        if(_playerDirection > 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-            faceRight = true;
-        }
-        else if(_playerDirection < 0)
-        {
-            transform.localScale = Vector3.one;
-            faceRight = false;
-        }
-    }
+    #endregion
 }
