4ef88e31cc8e8a7a8369f0bae5cf33f4d9a5d13f
chopin890612 <chopin890612@gmail.com>
2022-10-31 19:53:43 +0800

    Fire Ver0.1

--- Dream/Assets/Scripts/CharactorUnit/Player/Fire.cs
+++ Dream/Assets/Scripts/CharactorUnit/Player/Fire.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    public FireSpot[] fireSpots;
+
+    private bool _inRange = false;
+    [SerializeField] private int _maxSpotCount;
+    [SerializeField] private int _currntSpotCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _maxSpotCount = fireSpots.Length;
+        _currntSpotCount = 0;
+        EventManager.instance.CollectFireEvent.AddListener(FireSpotCallback);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FireSpotCallback()
+    {
+        _currntSpotCount++;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            _inRange = true;
+
+        Debug.Log("123");
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            _inRange = false;
     }
 }
---
+++ Dream/Assets/Scripts/CharactorUnit/Player/FireSpot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            EventManager.instance.CollectFireEvent.Invoke();
+    }
+}
